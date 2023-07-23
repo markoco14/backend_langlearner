8da86aa2465228ef22a1baa6b753cf5d6a57b33f
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-24 07:04:40 +0800

    feature: set up mysql database

--- backend_langlearner/settings.py
+++ backend_langlearner/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'backend_langlearner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DEV_DB_NAME'),
+        'USER': os.environ.get('DEV_DB_USER'),
+        'PASSWORD': os.environ.get('DEV_DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
