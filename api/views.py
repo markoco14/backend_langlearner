590c7952f6fd795572bd8a544aa4b82c1d5c45c6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-24 11:41:13 +0800

    feature: make write-post api view and PostSerializer in api module

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from api.serializers import PostSerializer
+from posts.models import Post
+
 # Create your views here.
 @api_view(['GET'])
 def helloWorld(request):
@@ -8,5 +11,11 @@ def helloWorld(request):
 
 @api_view(['POST'])
 def write_post(request):
-    post = request.data
-    return Response(post)
+    serializer = PostSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response("something went wrong")
+    
