204bc1b3c9665c6c60263cb563d6b260e2723c36
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-24 11:56:56 +0800

    feature: add PostContent model to posts module

--- posts/models.py
+++ posts/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class Post(models.Model):
     title=models.CharField(max_length=100, default="")
+    
+class PostContent(models.Model):
+    content=models.TextField()
+    level=models.CharField(max_length=50, default="")
+    post=models.ForeignKey(Post, related_name='posts_content', on_delete=models.CASCADE)
+    class Meta:
+        unique_together=['level', 'post']
