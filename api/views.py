621170bb8dfaa58430635d19785bea4af63f907b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-25 14:07:12 +0800

    feature: install google-cloud-storage

--- api/views.py
+++ api/views.py
@@ -141,14 +141,8 @@ def get_tts(request, pk):
     post_content = PostContent.objects.get(id=pk)
     content = post_content.content
 
-    # Instantiates a client
     client = texttospeech.TextToSpeechClient()
-
-    # Set the text input to be synthesized
     synthesis_input = texttospeech.SynthesisInput(text=content)
-
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
     # MALE VOICE
     voice = texttospeech.VoiceSelectionParams(
         language_code="cmn-TW",
@@ -161,29 +155,26 @@ def get_tts(request, pk):
     #     name="cmn-TW-Wavenet-A",
     #     ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
     # )
-
-    # Select the type of audio file you want returned
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.MP3
     )
-
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
     response = client.synthesize_speech(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
-
-    # The response's audio_content is binary.
+    # REPLACE WITH CLOUD STORAGE
     with open("./files/male.mp3", "wb") as out:
         # Write the response to the output file.
         out.write(response.audio_content)
 
-    return Response('Audio content written to ./files as "output.mp3"')
+    return Response('Audio content written to ./files as "male.mp3"')
 
 
 # what are the steps
 # 1) choose a title for the article (can change later)
 # 2) make the article content
 # 3) get the voice
-# 4) get the pinyin
-# 5) get the segments
+# 4) set up storage
+# 5) get the pinyin
+# 6) get the segments
+# 7) get the timestamps (update voice)
+# 8) get the dictionary (because you can make copy/paste/translate easily for now)
