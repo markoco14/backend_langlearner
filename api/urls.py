40b6d642be5b1bb3c0e41471cb42947b0dc7d4c3
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-05 13:43:52 +0800

    feature: set up basic get audio url path

--- api/urls.py
+++ api/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('posts/content/<str:pk>/segments/', views.get_segments, name='get-segments'),
     path('posts/content/<str:pk>/audio/', views.get_tts, name='get-audio'),
 
-
-    path('posts/content/get-audio/', views.get_audio_from_bucket, name='get-audio-from-bucket')
+    path('posts/content/get-audio/', views.get_audio_from_bucket, name='get-audio-from-bucket'),
+    path('posts/content/get-audio-url/', views.get_audio_url, name='get-audio-url'),
 
 ]
