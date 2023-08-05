a4ce1f99d433d074148ab4e8acad5c1166bbc402
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-05 12:48:57 +0800

    refactor: update endpoint urls to RESTful style

--- api/urls.py
+++ api/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
 
     path('posts/', views.get_posts, name='get-posts'),
     path('posts/write/', views.write_post, name='write-post'),
-    path('posts/update/<str:pk>/', views.update_post, name='update-post'),
-    path('posts/delete/<str:pk>/', views.delete_post, name='delete-post'),
+    path('posts/<str:pk>/update/', views.update_post, name='update-post'),
+    path('posts/<str:pk>/delete/', views.delete_post, name='delete-post'),
 
     path('posts/content/', views.get_post_contents, name='get-post-contents'),
     path('posts/<str:post_pk>/content/write/', views.write_post_content, name='write-post-content'),
-    path('posts/content/update/<str:pk>/', views.update_post_content, name='update-post-content'),
-    path('posts/content/delete/<str:pk>/', views.delete_post_content, name='delete-post-content'),
+    path('posts/content/<str:pk>/update/', views.update_post_content, name='update-post-content'),
+    path('posts/content/<str:pk>/delete/', views.delete_post_content, name='delete-post-content'),
 
     path('posts/content/<str:pk>/pinyin/', views.get_post_pinyin, name='get-post-pinyin'),
     path('posts/content/<str:pk>/segments/', views.get_segments, name='get-segments'),
