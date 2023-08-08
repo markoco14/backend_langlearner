3db76856d912a3ac5ff1991e2a6035bc4210f5f2
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-08 12:17:41 +0800

    feature: update Post and PostContent models and refresh migrations

--- posts/models.py
+++ posts/models.py
@@ -2,14 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
-    title=models.CharField(max_length=100, default="")
-    
+    title = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.title
 
 
-    
 class PostContent(models.Model):
-    content=models.TextField()
-    post=models.ForeignKey(Post, related_name='posts_content', on_delete=models.CASCADE)
-    class Meta:
-        unique_together=['post']
+    post = models.ForeignKey(
+        Post, related_name='content', on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Content for {self.post.title}"
+
+
+# class PostContentPinyin(models.Model):
+#     pinyin_content = models.TextField()
+#     post_content = models.OneToOneField(
+#         PostContent, related_name='content_pinyin', on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f"Pinyin for {self.post_content.content}"
