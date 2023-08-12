c2fcec1c6f548810cf21740789c7839eb7f72298
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-12 10:35:57 +0800

    chore: format serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,24 +2,25 @@ from rest_framework import serializers
 
 from posts.models import Post, PostContent, PostContentPinyin
 
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Post
-        fields='__all__'
-        
+        model = Post
+        fields = '__all__'
+
+
 class PostContentSerializer(serializers.ModelSerializer):
-		class Meta:
-				model=PostContent
-				fields='__all__'
-                                
+    class Meta:
+        model = PostContent
+        fields = '__all__'
+
 
 class PostContentPinyinSerializer(serializers.ModelSerializer):
     class Meta:
-        model=PostContentPinyin
-        fields='__all__'
+        model = PostContentPinyin
+        fields = '__all__'
+
 
 class ReadPostSerializer(serializers.Serializer):
     class Meta:
-        fields='id, content, pinyin, title'
-
-
+        fields = 'id, content, title'
