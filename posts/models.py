b462993567d069519af5128ca37a0a9f555d6b12
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-12 12:16:49 +0800

    feature: set post title to unique

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, default="")
+    title = models.CharField(max_length=100, default="", unique=True)
 
     def __str__(self):
         return f"{self.title} (id: {self.id})"
