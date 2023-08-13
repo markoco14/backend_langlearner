a830cf7f8c266c3192d1a7cdce835cc4009f59a3
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-13 14:17:41 +0800

    refactor: move Post Content model CRUD functions from views.py to post_content_views.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import views, post_views
+from .views import views, post_views, post_content_views
 
 urlpatterns = [
     # GREETING ROUTE
@@ -10,11 +10,11 @@ urlpatterns = [
     path('posts/<str:pk>/update/', post_views.update_post, name='update-post'),
     path('posts/<str:pk>/delete/', post_views.delete_post, name='delete-post'),
 
-    path('posts/<str:post_pk>/content/', views.get_post_content, name='get-post-content'),
-    path('posts/<str:post_pk>/content/level/<str:level_pk>/', views.get_post_content_by_id_level, name='get-post-content-id-level'),
-    path('posts/<str:post_pk>/content/write/', views.write_post_content, name='write-post-content'),
-    path('posts/content/<str:pk>/update/', views.update_post_content, name='update-post-content'),
-    path('posts/content/<str:pk>/delete/', views.delete_post_content, name='delete-post-content'),
+    path('posts/<str:post_pk>/content/', post_content_views.get_post_content, name='get-post-content'),
+    path('posts/<str:post_pk>/content/level/<str:level_pk>/', post_content_views.get_post_content_by_id_level, name='get-post-content-id-level'),
+    path('posts/<str:post_pk>/content/write/', post_content_views.write_post_content, name='write-post-content'),
+    path('posts/content/<str:pk>/update/', post_content_views.update_post_content, name='update-post-content'),
+    path('posts/content/<str:pk>/delete/', post_content_views.delete_post_content, name='delete-post-content'),
 
     path('posts/<str:pk>/content/pinyin/', views.get_pinyin_content_by_post_id, name='get-pinyin-content'),
     path('posts/content/<str:pk>/pinyin/create/', views.create_post_pinyin, name='get-post-pinyin'),
