30500fd9e5aa960641abfa60b4e642181c92e316
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-30 21:13:34 +0800

    fix: some changes to add ce dict words script

--- cedict/management/commands/parse_cc_cedict.py
+++ cedict/management/commands/parse_cc_cedict.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
         
         if line[0] == '%':
             return 0
-        # print('base line', line)
+        
         # INITIATE WORD OBJECT TO HOLD DATA
         wordObject = {}
         if line == '':
@@ -23,22 +23,13 @@ class Command(BaseCommand):
         length = len(split_line)
         if length != 2:
             lines_not_2 = lines_not_2 + 1
-        # print('split on / line', split_line)
-        # print('length of line', length)
+   
         characters = split_line[0]
-        # print('characters:', characters)
-        trad_simp_pin = split_line[0].split(' [')
-        # print('trad_simp_pin:', trad_simp_pin)
+        trad_simp_pin = characters.split(' [')
         traditional = trad_simp_pin[0].split(" ")[0]
-        # print('traditional:', traditional)
         simplified = trad_simp_pin[0].split(" ")[1]
-        # print('simplified:', simplified)
         pinyin = trad_simp_pin[1].rstrip(']')
-        # print('pinyin:', pinyin)
         english = split_line[1]
-        # print('english:', english)
-        if row == 181:
-            print(simplified)
         
 
         # APPEND STORED VARIABLES INTO HOLDER OBJECT (wordObject)
@@ -77,29 +68,19 @@ class Command(BaseCommand):
         print('lines longer than 2:', lines_not_2)
         print('number of lines:', number_of_lines)
 
-        self.stdout.write("Removing Surnames . . .")
-        self.remove_surnames(list_of_words)
-
-
         self.stdout.write(self.style.SUCCESS('Done!'))
         read_end_time = time.time()
         read_duration = read_end_time - read_start_time
 
         write_start_time = time.time()
-        # Optional: To save to the Django database (ensure model is created & imported)
-        # self.stdout.write("Saving to database (this may take a few minutes) . . .")
-        # for one_dict in list_of_words:
-        #     new_word = Word(traditional=one_dict["traditional"], 
-        #                     simplified=one_dict["simplified"], 
-        #                     english=one_dict["english"], 
-        #                     pinyin=one_dict["pinyin"])
-        #     new_word.save()
-        # words = [Word(traditional=word["traditional"], 
-        #       simplified=word["simplified"], 
-        #       english=word["english"], 
-        #       pinyin=word["pinyin"]) for word in list_of_words]
 
-        # Word.objects.bulk_create(words, batch_size=5000)
+        # WRITE TO DB
+        words = [Word(traditional=word["traditional"], 
+              simplified=word["simplified"], 
+              english=word["english"], 
+              pinyin=word["pinyin"]) for word in list_of_words]
+
+        Word.objects.bulk_create(words, batch_size=5000)
 
         write_end_time = time.time()
         write_duration = write_end_time - write_start_time
